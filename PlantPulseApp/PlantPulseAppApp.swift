7d0835f0d3c5d563ed9f55a94cca1f0224c6b59d
Mohammed Shaheen <102543131+mohammedshaheen187@users.noreply.github.com>
2023-11-21 13:00:33 +0100

    Initial Commit

---
+++ PlantPulseApp/PlantPulseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantPulseAppApp.swift
+//  PlantPulseApp
+//
+//  Created by Mohammed Shaheen on 21.11.23.
+//
+
+import SwiftUI
+
+@main
+struct PlantPulseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
