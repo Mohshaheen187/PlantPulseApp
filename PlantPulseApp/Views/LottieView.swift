6cec76f6a01565a6e01a698ca4d501c39b2d35ca
Mohammed Shaheen <102543131+mohammedshaheen187@users.noreply.github.com>
2023-11-22 13:45:50 +0100

    Imported lotties and created the file

---
+++ PlantPulseApp/Views/LottieView.swift
@@ -0,0 +1,36 @@
+//
+//  LottieView.swift
+//  PlantPulseApp
+//
+//  Created by Mohammed Shaheen on 22.11.23.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView : UIViewRepresentable {
+    
+    let lottieFile : String
+    
+    let animationView = LottieAnimationView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView(frame: .zero)
+        
+        animationView.animation = LottieAnimation.named(lottieFile)
+        animationView.contentMode = .scaleAspectFit
+        animationView.play()
+        
+        view.addSubview(animationView)
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        animationView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
